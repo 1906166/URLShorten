7ac3a1296f347a6677a5afc2267526cdc1d009cb
Saurabh Patil <saurabhp.ug19.cs@nitp.ac.in>
2026-07-02 20:23:02 +0530

    Initial commit

---
+++ src/main/java/code/practice/URLShorten/UrlShortenApplication.java
@@ -0,0 +1,12 @@
+package code.practice.URLShorten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenApplication.class, args);
+	}
+}
---
+++ src/test/java/code/practice/URLShorten/UrlShortenApplicationTests.java
@@ -0,0 +1,13 @@
+package code.practice.URLShorten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
